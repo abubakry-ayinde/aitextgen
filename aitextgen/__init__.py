32cd732bc75d547f0bf314fa0ebcc4a980146f8c
abubakry-ayinde <abubakryadedamola@gmail.com>
2024-05-29 11:35:05 +0100

    Update __init__.py

--- aitextgen/__init__.py
+++ aitextgen/__init__.py
@@ -1,1 +1,4 @@
 from .aitextgen import aitextgen  # noqa
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks.progress import ProgressBarBase
+from pytorch_lightning.accelerators import TPUAccelerator
